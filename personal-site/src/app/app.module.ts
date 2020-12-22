ac8f16a2a95080abbfd718e72711e56d28de3f88
AnnieisOK <morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.com>
2020-12-22 23:43:08 +0200

    initial setup

--- personal-site/src/app/app.module.ts
+++ personal-site/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavigationComponent } from './components/nav/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
